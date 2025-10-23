6c7e57195573fcb8033a75eb31629d17a91e199d
Dat Nguyen <tdat.nguyen93@gmail.com>
2025-10-23 09:01:02 -0700

    added scale test

---
+++ scale.py
@@ -0,0 +1,20 @@
+from tests.adapters import run_train_bpe
+
+def main():
+    input_path = "./tests/fixtures/corpus.en"
+    vocab, merges = run_train_bpe(
+        input_path=input_path,
+        vocab_size=500,
+        special_tokens=["<|endoftext|>"],
+    )
+
+    # Check that the special token is not in the vocab
+    vocabs_without_specials = [word for word in vocab.values() if word != b"<|endoftext|>"]
+    for word_bytes in vocabs_without_specials:
+        assert b"<|" not in word_bytes
+
+    print(len(merges))
+    print(merges[:10])
+
+if __name__ == "__main__":
+    main()
