27cd2999c335ef8ddfe663f6f394e52cbd9d4934
Dat Nguyen <tdat.nguyen93@gmail.com>
2026-06-27 09:04:04 -0700

    BPE is cooked

--- train_tiny_stories.py
+++ train_tiny_stories.py
@@ -1,9 +1,13 @@
+"""
+TinyStories V2 GPT4: time 151.04 seconds, longest sequence is Ġaccomplishment, merge step takes the most time (97/151s)
+
+"""
 import argparse
 import json
 import logging
 import time
 
-from cs336_basics.bpe import run_train_bpe
+from cs336_basics.train import run_train_bpe
 from tests.common import gpt2_bytes_to_unicode
 logger = logging.getLogger(__name__)
 
@@ -30,21 +34,29 @@ def main():
     logging.info("Starting BPE training")
     start = time.perf_counter()
     vocab, merges = run_train_bpe(
+        input_path="./data/owt_train.txt",
+        vocab_size=32000,
         # input_path="./data/TinyStoriesV2-GPT4-train.txt",
-        input_path="./tests/fixtures/tinystories_sample_5M.txt",
-        vocab_size=500,
+        # vocab_size=10000,
+        # input_path="./tests/fixtures/tinystories_sample_5M.txt",
+        # vocab_size=2000,
         special_tokens=["<|endoftext|>"]
     )
     elapsed = time.perf_counter() - start
 
     # writing vocab merges to disk
-
-    with open("./vocab.json", "w") as f:
+    with open("vocab.json", "w") as f:
        json.dump({decode(bytestring): k for k, bytestring in vocab.items()}, f, indent=4, ensure_ascii=False) 
 
+    with open("merges.txt", "w") as f:
+        for (bs1, bs2) in merges:
+            f.write(decode(bs1) + " " + decode(bs2) +"\n")
+
+    longest_token = max(vocab.values(), key=lambda token: len(token))
     logger.info("Finished BPE training in %.2f seconds", elapsed)
     logger.info("Vocab size: %d", len(vocab))
     logger.info("Number of merges: %d", len(merges))
+    logger.info(f"Longest token is : {decode(longest_token)}")
 
     ## compute compression stats
 
