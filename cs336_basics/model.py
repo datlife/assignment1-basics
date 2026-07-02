c9e30d96f5122de598d7c0f13360949938a54701
Dat Nguyen <tdat.nguyen93@gmail.com>
2026-07-02 07:54:30 -0700

    updated

--- cs336_basics/model.py
+++ cs336_basics/model.py
@@ -1,25 +1,30 @@
 import math
 import torch
-from jaxtyping import Bool, Float, Int
+from jaxtyping import Float
+from typing import Any
 from torch import nn
-from typing import List
 
 
 class LinearLayer(nn.Module):
-    def __init__(self, in_features: int, out_features: int, device=None, dtype=None) -> None:
+    def __init__(
+        self, in_features: int, out_features: int, device=None, dtype=None
+    ) -> None:
         super().__init__()
-        self.weights = nn.Parameter(torch.empty((in_features, out_features), device=device, dtype=dtype))
-        
+        self.weights = nn.Parameter(
+            torch.empty((in_features, out_features), device=device, dtype=dtype)
+        )
+
         # Xavier initialization to avoid vanishing / exploding gradients
-        std = math.sqrt(2/(in_features + out_features))
-        nn.init.trunc_normal_(self.weights, mean=0.0, std=std,  a=-3*std, b=3*std)
+        std = math.sqrt(2 / (in_features + out_features))
+        nn.init.trunc_normal_(self.weights, mean=0.0, std=std, a=-3 * std, b=3 * std)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return torch.matmul(x, self.weights)
-    
-    def set_weights(self, weights: Float[Tensor, "d_out d_in"]):
+
+    def set_weights(self, weights: Float[torch.Tensor, "d_out d_in"]):  # noqa: F722
         with torch.no_grad():
             self.weights.copy_(weights.T)
+
 
 class EmbeddingLayer(nn.Module):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
@@ -27,4 +32,3 @@ class EmbeddingLayer(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         raise NotImplementedError
-    
